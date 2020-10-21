12a99cf8adc4b80d07b2b13a40a29fd3238969e6
Ramon Calvo <ramon.calvo@gmail.com>
2020-10-20 23:05:02 -0500

    Task #175358461 - Roles Create & Edit: make responsive - fine-tune responsive styles - fine-tune mobile draggable component - use the same list component for mode longView and not longView

--- client/src/app/role/list.vue
+++ client/src/app/role/list.vue
@@ -20,29 +20,56 @@ div
 							.role-aggregated Role(s) Aggregated
 							.allmighty Is Allmighty?
 							.enabled Enabled?
+							.actions Actions
 
-						draggable(v-model='roles', :options="{draggable:'.role'}",  @end="roleMoved")
-							.role(v-for="role in roles")
+						draggable(v-model='roles', :options="{draggable: longView ? '.role' : '.none'}",  @end="roleMoved" :class="{ 'responsive-list': !longView }")
+							.role(v-for="role in roles" :class="{ responsive: !longView }")
 								.element
 									table.roles-list
 										tr
+											h5(v-if="!longView") Name/Description:
 											td.name
 												span.role-title {{ role.name }}
 												br
 												| {{ role.description }}
-											td.allmighty
+
+											h5(v-if="!longView") Role(s) Aggregated:
+											td.addRole
+												// This functionality does not exist yet
+												span
+												| Role added
+
+											td.allmighty(v-if="longView")
 												span.fa.fa-check-square(v-if='role.isAlmighty')
 												| &nbsp
-											td.enabled
+											td.enabled(v-if="longView")
 												span.fa.fa-check-square(v-if='role.enabled')
 												| &nbsp
-											td.options
+											td.options(v-if="longView")
 												router-link.action-button(:to="{name: 'roleEdit', params: {roleName: role.name}}")
 													span.fa.fa-pencil.fa-lg
 												a.action-button(@click='openDeleteRoleDialog(role.name)')
 													span.fa.fa-trash.fa-lg
 
-						.text-left
+											h4(v-if="!longView")
+											td.allmighty(v-if="!longView")
+												span.label.label-success(v-if='role.isAlmighty') Allmighty
+												span.label.label-default(v-if='!role.isAlmighty') No allmighty
+
+											h4(v-if="!longView")
+											td.enabled(v-if="!longView")
+												span.label.label-success(v-if='role.enabled') Enabled
+												span.label.label-default(v-if='!role.enabled') No enabled
+
+											h4(v-if="!longView")
+											td.options(v-if="!longView")
+												button.edit-btn(type="button" class="btn btn-success" aria-haspopup="true" aria-expanded="false")
+													router-link.md-primary(:to="{name: 'roleEdit', params: {roleName: role.name}}") Edit
+
+												button.delete-btn(type="button" class="btn btn-danger" aria-haspopup="true" aria-expanded="false")
+													a.action-button(@click='openDeleteRoleDialog(role.name)') Delete
+
+						.text-left.add-role
 							router-link.link.primary(:to="{name: 'roleCreate'}")
 								span.fa.fa-plus.fa-lg
 								| &nbsp;Add Role
@@ -71,6 +98,8 @@ export default {
 	name: 'role-list',
 	data () {
 		return {
+			longView: false,
+			widthBrowser: 0,
 			sectionTitle: this.$t('permission.title'),
 			roles: [],
 			snackbar: { show: false, message: '', duration: 4000 },
@@ -83,6 +112,15 @@ export default {
 		Vue.jnx.roleService.findAll().then((response) => {
 			next(vm => (vm.roles = response))
 		})
+	},
+	created () {
+		this.isShowResponsiveData()
+	},
+	mounted () {
+		window.addEventListener('resize', this.isShowResponsiveData)
+	},
+	destroyed () {
+		window.removeEventListener('resize', this.isShowResponsiveData)
 	},
 	methods: {
 		roleMoved () {
@@ -110,6 +148,16 @@ export default {
 			Vue.jnx.roleService.deleteByName(this.roleNameToDelete).then(() => {
 				this.$root.$router.go()	// Reload
 			})
+		},
+		isShowResponsiveData () {
+			let widthPage = document.documentElement.clientWidth
+			console.log('widthPage ', widthPage)
+			this.widthBrowser = widthPage
+			if (widthPage >= 760) {
+				this.longView = true
+			} else {
+				this.longView = false
+			}
 		}
 	},
 	computed: mapState({
